1c07727d1a7c3fae038770671b00088a13f3ea72
Cleve716 <cleve716@msn.com>
2019-07-01 16:24:24 -0600

    added HTML routes for, and, basic HTML code to, category pages

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,9 +1,9 @@
 var db = require("../models");
 
-module.exports = function(app) {
+module.exports = function (app) {
   // Load index page
-  app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
+  app.get("/", function (req, res) {
+    db.Example.findAll({}).then(function (dbExamples) {
       res.render("index", {
         msg: "The killer clown apocalypse storE",
         examples: dbExamples
@@ -11,9 +11,33 @@ module.exports = function(app) {
     });
   });
 
+  app.get("/food", function (req, res) {
+    db.Example.findAll({ where: { category: "food" } }).then(function (dbExamples) {
+      res.render("food", {
+        examples: dbExamples
+      });
+    });
+  });
+
+  app.get("/gear", function (req, res) {
+    db.Example.findAll({ where: { category: "gear" } }).then(function (dbExamples) {
+      res.render("gear", {
+        examples: dbExamples
+      });
+    });
+  });
+  app.get("/weapons", function (req, res) {
+    db.Example.findAll({ where: { category: "weapons" } }).then(function (dbExamples) {
+      res.render("weapons", {
+        examples: dbExamples
+      });
+    });
+  });
+
+
   // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
+  app.get("/example/:id", function (req, res) {
+    db.Example.findOne({ where: { id: req.params.id } }).then(function (dbExample) {
       res.render("example", {
         example: dbExample
       });
@@ -21,7 +45,7 @@ module.exports = function(app) {
   });
 
   // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
+  app.get("*", function (req, res) {
     res.render("404");
   });
 };
