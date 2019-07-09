1ee569e1da1bba0be92409a17c7aa6595cf15a9f
Cleve716 <cleve716@msn.com>
2019-07-08 18:21:04 -0600

    cart.hbs and delete.js changes

--- public/js/delete.js
+++ public/js/delete.js
@@ -1,20 +1,19 @@
 
-var API = {
-    deleteCart: function (id) {
-      return $.ajax({
-        url: "api/carts/" + id,
-        type: "DELETE"
-      });
-    }
-  };
-  
-  var handleDeleteBtnClick = function () {
-    var idToDelete = $(this).parent().attr("data-id");
-  
-    API.deleteCart(idToDelete).then(function () {
-      location.reload();
+
+
+  $(".delete").on("click",function(){
+
+    var id = $(this).parents("tr").data("id");
+    
+    $.ajax({
+    
+    method: "DELETE",
+    url: `/api/carts/${id}`
+    
+    }).then(function(){
+    
+    location.reload();
+    
     });
-  };
-  
-  $("#example-list").on("click", ".delete", handleDeleteBtnClick);
-  
+    
+    });
